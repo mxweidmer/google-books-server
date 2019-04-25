a22953be94fdd187967695875d27819bf8a0e0d7
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-25 15:11:09 -0700

    cors stuff

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -5,7 +5,6 @@ module.exports = {
   findSaved: function (req, res) {
     db.Book
       .find({})
-      .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.json());
 // Add routes, both API and view
 app.use(routes);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 

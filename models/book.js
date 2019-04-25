561bd0ebcc05a7e8e3129e7c19b70b99c8e49077
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-25 11:33:54 -0700

    moving server files into new repo

---
+++ models/book.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  thumbnail: { type: String, default: "https://via.placeholder.com/150" }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;

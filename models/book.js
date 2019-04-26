d1fe92b157badb6534b4d364d5f049c18e8cd153
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-26 13:02:27 -0700

    adding infolink to model

--- models/book.js
+++ models/book.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: [{ type: String, required: true }],
+  author: [{ type: String }],
   synopsis: String,
-  thumbnail: { type: String, default: "https://via.placeholder.com/150" }
+  thumbnail: { type: String, default: "https://via.placeholder.com/150" },
+  link: String
 });
 
 const Book = mongoose.model("Book", bookSchema);

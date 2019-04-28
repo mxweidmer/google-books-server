3d36d0af79b31c3f1a2d8c7d4022628bf266b186
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-28 13:40:29 -0700

    fixing model

--- models/book.js
+++ models/book.js
@@ -5,7 +5,7 @@ const bookSchema = new Schema({
   title: { type: String, required: true },
   author: [{ type: String }],
   synopsis: String,
-  thumbnail: { type: String },
+  cover: { type: String },
   link: String
 });
 
